a9affee97c0f0c80590cc5857e44ac1de1f71943
judagonsa <judagonsa8@gmail.com>
2024-06-25 13:02:32 -0500

    Create init project

---
+++ SkilibraSpa/SkilibraSpaApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkilibraSpaApp.swift
+//  SkilibraSpa
+//
+//  Created by Julian González on 25/06/24.
+//
+
+import SwiftUI
+
+@main
+struct SkilibraSpaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
